5543c31270835ea593b6fcde59e0ef39474cc554
vpavlovic <vpavlovic@westum.com>
2014-08-15 11:46:19 +0200

    TST00 Edit

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <meta charset="utf-8" />
 <head>
-    <title>Vladimir</title>
+    <title>Vladimir Sandbox</title>
 </head>
 <body>
     <h1>Boilerplate</h1>
+    <p>Edit</p>
 </body>
 </html>
