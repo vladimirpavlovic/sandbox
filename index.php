f86dcac6b4a6d063b68b344d465b9b0ae766f399
vpavlovic <vpavlovic@westum.com>
2014-09-09 09:53:44 +0200

    FEAT2 - Second

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 <meta charset="utf-8" />
 <head>
-    <title>Vladimir Sandbox - Lipsum Boilerplate</title>
+    <title>Vladimir Sandbox - Lipsum Boilerplate Feature</title>
 </head>
 <body>
     <h1>Boilerplate</h1>
